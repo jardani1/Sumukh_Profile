05e10506454d868b99d43d9cd1ddfbc9604822f9
Sumukh <srivastava.sumukh8808@gmail.com>
2024-06-20 01:57:37 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// var name = undefined;
+// let name2 = undefined;
+// console.log("hello brother")
+var count = 1;   
+let age=50;
+function sum(a, b, c){ 
+var count = 0;   
+return a + b + c;  
+} 
+
+if(age>18){ 
+// var count = 2; 
+    console.log(count) 
+}
